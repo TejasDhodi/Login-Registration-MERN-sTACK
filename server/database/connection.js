0d5ee6932773c7e96151f7e157cbfd5962fd748e
Tejas Dhodi <tejasdhodi77@gmail.com>
2023-07-30 18:21:05 +0530

    minor changes

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose")
-
-mongoose.connect(process.env.MONGO_URL).then(() => {
+const dotenv = require("dotenv")
+dotenv.config({path: "../.env"})
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
     console.log("connection established");
+}).catch((err) => {
+    console.log("failed to connect");
 })
